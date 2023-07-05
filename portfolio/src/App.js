6da8c7167a96f5c2d7b0b6a45d9e2a611b1a7eb6
lais-cardoso <lalaicardoso27@hotmail.com>
2023-07-05 20:42:11 -0300

    Configurando fontes necessárias para o projeto

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './home/index';
+import {createTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
+  const theme = createTheme({
+    typhography:{
+      fontFamily: [
+        'Roboto',
+        'Oswald',
+        '"sans-serif"',
+        'Liu Jian Mao Cao',
+        'Fasthand',
+        'Cedarville Cursive', 
+        '"cursive"',
+        'EB Garamond',
+        '"serif"'
+      ].join(','),
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home/>
+    </ThemeProvider>
   );
 }
 
