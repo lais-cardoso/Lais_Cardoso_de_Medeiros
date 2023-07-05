e6a7613a93b3506a224fcdbc09a7300406198a12
lais-cardoso <lalaicardoso27@hotmail.com>
2023-07-04 23:48:35 -0300

    Grid para os projetos

--- portfolio/src/home/index.jsx
+++ portfolio/src/home/index.jsx
@@ -1,8 +1,17 @@
 import styles from './styles';
 import './styles.js';
 import React from 'react';
-import { Container, Button, Grid, TextField, CardContent, CardActions, Box, Typography, Avatar, ThemeProvider } from '@material-ui/core/';
+import { Container, Button, Grid, TextField, CardContent, CardActions, Box, Typography, Avatar, Paper } from '@material-ui/core/';
+import { experimentalStyled as styled } from '@mui/material/styles';
 //icons-material nao instalou
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function Home() {
     return (
@@ -69,6 +78,16 @@ function Home() {
                         <Button size="small">Contato</Button>
                     </CardActions>
                 </Grid>
+
+                <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            <Item>xs=2</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
             
         </Container>
     );
