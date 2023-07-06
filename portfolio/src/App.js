d861adc5668390dd193a1ab7cbb1e747d2cf232b
lais-cardoso <lalaicardoso27@hotmail.com>
2023-07-05 22:46:48 -0300

    view principal

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -20,7 +20,7 @@ function App() {
     }, 
     palette: {
       primary: {
-        light: '#FFFFFF',
+        light: '#757ce8',
         main: '#3f50b5',
         dark: '#002884',
         contrastText: '#fff',
